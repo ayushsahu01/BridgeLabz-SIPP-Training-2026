14dc073adf80e1d9d77d1824a10aa34e51671be2
ayushsahu01 <ayushsahu29042005@gmail.com>
2026-06-09 10:10:49 +0530

    [Ayush] Add: Scenerio Based Question Day 1 08062026

---
+++ core-java-practice/gcr-codebase/programming-elements/Day 1(080626)/ScenerioBasedQuestion.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ScenerioBasedQuestion {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hii Ravi, Please Share Your Details Below");
+        System.out.print("Enter Your Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter Your Age: ");
+        int age = sc.nextInt();
+        System.out.print("Enter Your Rank:");
+        int rank = sc.nextInt();
+        System.out.print("Enter your Salary: ");
+        int salary = sc.nextInt();
+        System.out.print("Enter Your Fees: ");
+        float fees = sc.nextFloat();
+        float bonus = ((12f/100)*salary);
+        int bonus1 = (int)(bonus+salary);
+        System.out.println("Your Bonus is: "+ bonus1);
+        sc.close();
+    }
+}
