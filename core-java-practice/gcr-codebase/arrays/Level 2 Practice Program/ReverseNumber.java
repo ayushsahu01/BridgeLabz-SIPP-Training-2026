ef60353eacced870fcb4397de484d0cea6bd1ccf
ayushsahu01 <ayushsahu29042005@gmail.com>
2026-06-10 11:10:49 +0530

    [Ayush] Add: Level 1 and 2 Practice Program Arrays

---
+++ core-java-practice/gcr-codebase/arrays/Level 2 Practice Program/ReverseNumber.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int temp = number;
+
+        int count = 0;
+
+        while (temp != 0) {
+            count++;
+            temp = temp / 10;
+        }
+
+        int[] digits = new int[count];
+
+        temp = number;
+
+        for (int i = 0; i < count; i++) {
+
+            digits[i] = temp % 10;
+
+            temp = temp / 10;
+        }
+
+        int[] reverse = new int[count];
+
+        int index = 0;
+
+        for (int i = count - 1; i >= 0; i--) {
+
+            reverse[index] = digits[i];
+
+            index++;
+        }
+
+        System.out.println("Reversed Number:");
+
+        for (int i = 0; i < reverse.length; i++) {
+            System.out.print(reverse[i]);
+        }
+        sc.close();
+    }
+}
