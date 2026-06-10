6b16caacf253e157c3b9a4b47edeed43cba4d8e8
ayushsahu01 <ayushsahu29042005@gmail.com>
2026-06-10 11:49:44 +0530

    [Ayush] Add: Level 1 and 2 Practice Program Programming Elements

---
+++ core-java-practice/gcr-codebase/programming-elements/Level 2 Practice Programs/QuotientAndRemainder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class QuotientAndRemainder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+
+        int quotient = number1 / number2;
+        int reminder = number1 % number2;
+
+        System.out.println("The Quotient is " + quotient
+                + " and Reminder is " + reminder
+                + " of two number " + number1
+                + " and " + number2);
+        input.close();
+    }
+}
